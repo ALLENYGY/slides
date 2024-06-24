66012967cce64ecdebca7ea9702302a15d48c6c6
AllenYGY <allenygy@outlook.com>
2024-06-24 11:23:40 +0800

    TEST

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Slides仓库
 
-使用[Slidev](https://github.com/slidevjs/slidev)创建的[幻灯片仓库](https://github.com/open17/slides/tree/main)
+使用[Slidev](https://github.com/slidevjs/slidev)创建的[幻灯片仓库](https://github.com/ALLENYGY/slides/tree/main)
 
 每个幻灯片都会提供两个预览地址,github和vercel(对应本仓库的两个分支)
 
@@ -8,11 +8,9 @@
 
 github page为备用预览路线,不太稳定,且推荐海外路线访问
 
-- [Vercel](https://slides.open17.vip/)
-- [Github Page](https://open17.github.io/slides/)
+- [Vercel](https://slides.ALLENYGY.vip/)
+- [Github Page](https://ALLENYGY.github.io/slides/)
 
 <!-- start -->
 
-
 <!-- end -->
-
--- auto.py
+++ auto.py
@@ -12,7 +12,7 @@ print("文件夹:", folder)
 
 for f in folder:
     os.system(f"cd {current_directory} & npx slidev build ./src/{f}/slides.md --base /slides/{f}/ --out ../../dist/{f}/ ")
-    md_links+=f"\n## {f} \n- [vercel预览](https://slides.open17.vip/slides/{f}) \n- [github page预览](https://open17.github.io/slides/{f})  \n"
+    md_links+=f"\n## {f} \n- [vercel预览](https://slides.ALLENYGY.vip/slides/{f}) \n- [github page预览](https://ALLENYGY.github.io/slides/{f})  \n"
 
 
 
@@ -39,7 +39,7 @@ new_content = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Open17's Slides</title>
+    <title>ALLENYGY's Slides</title>
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.5.1/github-markdown-light.min.css'>
 </head>
 <body>
